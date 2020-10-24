a2bc04804f3707d9a52ef017e7c91641e1be2541
catfoot <nye7005@gmail.com>
2020-10-24 11:00:59 -0400

    lab3

--- tools/labs/skels/kernel_api/3-memory/memory.c
+++ tools/labs/skels/kernel_api/3-memory/memory.c
@@ -8,6 +8,7 @@
 #include <linux/slab.h>
 #include <linux/sched.h>
 #include <linux/sched/signal.h>
+#include <linux/list.h>
 
 MODULE_DESCRIPTION("Memory processing");
 MODULE_AUTHOR("SO2");
@@ -36,31 +37,30 @@ static struct task_info *task_info_alloc(int pid)
 
 static int memory_init(void)
 {
+	struct task_struct *nextProc;
+	struct task_struct *nextnextProc;
+
 	/* TODO 2: call task_info_alloc for current pid */
 	ti1 = task_info_alloc(current->pid);
 	
 	/* TODO 2: call task_info_alloc for parent PID */
-	ti2 = task_info_alloc(current->real_parent->pid);
-
-	struct list_head *i;
-    void *elem;
-
-	read_lock(&lock);
-    list_for_each(i, lh) {
-        struct pid_list *pl = list_entry(i, struct pid_list, list);
-        if (pl->pid == pid) {
-            read_unlock(&lock);
-            return 1;
-        }
-    }
-    read_unlock(&lock);
+	ti2 = task_info_alloc(current->parent->pid);
 
 	/* TODO 2: call task_info alloc for next process PID */
 	/*커널 링크드 리스트 공부하고 여기 풀기 */
-	//t3 = list_entry(t1->sibling, ,
 
-	/* TODO 2: call text_info_alloc for next process of the nex process */
+	nextProc = next_task(current->parent);
+	ti3 = task_info_alloc(nextProc->pid);
 
+	/* TODO 2: call text_info_alloc for next process of the next process */
+	nextnextProc = next_task(nextProc);
+	ti4 = task_info_alloc(nextnextProc->pid);
+
+	printk("ti1 pid = %d timestamp = %lu\n", ti1->pid, ti1->timestamp);
+	printk("ti2 pid = %d timestamp = %lu\n", ti2->pid, ti2->timestamp);
+	printk("ti3 pid = %d timestamp = %lu\n", ti3->pid, ti3->timestamp);
+	printk("ti4 pid = %d timestamp = %lu\n", ti4->pid, ti4->timestamp);
+	
 	return 0;
 }
 
